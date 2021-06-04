6653153c14f3e82fdae96171e4cdbe406b72a89e
Kenneth Olukotun <kenneth.olukotun@indicina.co>
2021-06-04 14:03:09 +0100

    added favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function App() {
-  return <div className="App">hello world!!</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Hello there</h1>
+    </div>
+  );
+};
 
 export default App;

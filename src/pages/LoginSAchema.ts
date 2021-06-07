b412fd145cb2d8e5297c7fdef375df774411293c
ken0063 <64601081+ken0063@users.noreply.github.com>
2021-06-07 08:12:29 +0100

    Delete src directory

--- src/pages/LoginSAchema.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup';
-
-export const LoginSAchema = yup.object().shape({
-  email: yup.string().email().required('Enter valid email'),
-  password: yup.string().required('Enter valid password'),
-});
